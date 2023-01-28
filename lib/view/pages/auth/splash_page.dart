3e1457b13bb98a0287c97813cbb1c1f2d4e23b86
jamshidmirzamaxmudov <jamshimirzamxmudov3@gmail.com>
2023-01-28 15:17:35 +0500

    Initial commit

---
+++ lib/view/pages/auth/splash_page.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+import 'login_page.dart';
+
+class SplashPage extends StatefulWidget {
+  const SplashPage({Key? key}) : super(key: key);
+
+  @override
+  State<SplashPage> createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> {
+  @override
+  void initState() {
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      Navigator.pushAndRemoveUntil(context,
+          MaterialPageRoute(builder: (_) => LoginPage()), (route) => false);
+    });
+
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text("Splash Screen"),
+      ),
+    );
+  }
+}
