0f0bdcfed586e7d039f0673f9dcb29a17c926090
jamshidmirzamaxmudov <jamshimirzamxmudov3@gmail.com>
2023-02-03 17:43:03 +0500

    - fixed bug chat

--- lib/controller/chat_controller.dart
+++ lib/controller/chat_controller.dart
@@ -64,7 +64,7 @@ class ChatController extends ChangeNotifier {
         ],
         "onlines": ownerIndex == 0
             ? [true, (element.data()["onlines"] as List)[1]]
-            : [(element.data()["onlines"] as List)[0],true]
+            : [(element.data()["onlines"] as List)[0], true]
       });
     }
     firestore
@@ -97,6 +97,7 @@ class ChatController extends ChangeNotifier {
     isLoading = true;
     notifyListeners();
     userId = await LocalStore.getDocId() ?? "";
+    messages.clear();
     firestore
         .collection("chats")
         .doc(docId)
@@ -121,11 +122,24 @@ class ChatController extends ChangeNotifier {
   }
 
   createChat(int index, ValueChanged<String> onSuccess) async {
-    var res = await firestore.collection("chats").add({
-      "ids": [listOfDocIdUser[index], await LocalStore.getDocId()],
-      "onlines": [false, true]
-    });
-    onSuccess(res.id);
+    List list = [];
+    for (var element in chats) {
+      list.addAll(element.ids);
+    }
+
+    if (!list.contains(listOfDocIdUser[index])) {
+      var res = await firestore.collection("chats").add({
+        "ids": [listOfDocIdUser[index], await LocalStore.getDocId()],
+        "onlines": [false, true]
+      });
+      onSuccess(res.id);
+    } else {
+      for (int i = 0; i < chats.length; i++) {
+        if (chats[i].ids.contains(listOfDocIdUser[index])) {
+          onSuccess(listOfDocIdChats[i]);
+        }
+      }
+    }
   }
 
   sendMessage(String title, String docId) async {
@@ -169,5 +183,25 @@ class ChatController extends ChangeNotifier {
           ).toJson());
     }
     editTime = null;
+  }
+
+  setOffline() async {
+    var res = await firestore
+        .collection("chats")
+        .where("ids", arrayContainsAny: [await LocalStore.getDocId()]).get();
+    for (var element in res.docs) {
+      int ownerIndex =
+          (element.data()["ids"] as List).indexOf(await LocalStore.getDocId());
+
+      firestore.collection("chats").doc(element.id).update({
+        "ids": [
+          (element.data()["ids"] as List)[0],
+          (element.data()["ids"] as List)[1]
+        ],
+        "onlines": ownerIndex == 0
+            ? [false, (element.data()["onlines"] as List)[1]]
+            : [(element.data()["onlines"] as List)[0], false]
+      });
+    }
   }
 }
